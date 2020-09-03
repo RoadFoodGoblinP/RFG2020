dff0d8760d7a9d916669cb41590a5f3df1667f63
jeeeun0227 <fjtldkwldms@naver.com>
2020-09-03 23:32:51 +0900

    0903_jieun

--- app/src/main/java/com/example/rfg2020/MainActivity.java
+++ app/src/main/java/com/example/rfg2020/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.rfg2020;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.map);
     }
 }
