811683cf25ed2a80b8b16ce09f99ee948c2da8f4
Kim-So-Hee <hseiz99@naver.com>
2020-09-17 23:31:50 +0900

    0917

---
+++ app/src/main/java/com/example/rfg2020/Muckpot_Detail.java
@@ -0,0 +1,14 @@
+package com.example.rfg2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Muckpot_Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muckpot__detail);
+    }
+}
